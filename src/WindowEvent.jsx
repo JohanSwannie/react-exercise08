1a32c591c741defe82849346322cab8959b4c46a
Johan Swan <jswannieman@gmail.com>
2024-01-01 11:09:25 +1300

    Update WindowEvent JSX Component

--- src/WindowEvent.jsx
+++ src/WindowEvent.jsx
@@ -1,12 +1,33 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const WindowEvent = () => {
+  const [counter, setCounter] = useState(0);
+
+  const addPElement = () => {
+    if (counter > 5) {
+      const deleteThem = document.querySelectorAll("children");
+      deleteThem.forEach((kid) => kid.remove());
+    } else {
+      const daddy = document.querySelector(".daddy");
+      const child = document.createElement("p");
+      child.setAttribute("class", "children");
+      child.innerHTML = "Another Child";
+      daddy.appendChild(child);
+      setCounter((prevCounter) => prevCounter + 1);
+    }
+  };
+
   useEffect(() => {
-    const doubleClick = () => alert("Mouse has been double clicked!");
+    const doubleClick = () => addPElement();
     window.addEventListener("dblclick", doubleClick);
     return () => window.removeEventListener("dblclick", doubleClick);
   });
-  return <div>Window Event is now Active</div>;
+
+  return (
+    <div className="daddy">
+      <p>Window Event is now Active - You may now double click</p>
+    </div>
+  );
 };
 
 export default WindowEvent;
