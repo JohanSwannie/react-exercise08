ab69fd97f82955461939636163b9507b7766beb5
Johan Swan <jswannieman@gmail.com>
2024-01-01 11:23:12 +1300

    Update WindowEvent JSX Component

--- src/WindowEvent.jsx
+++ src/WindowEvent.jsx
@@ -1,18 +1,18 @@
 import { useState, useEffect } from "react";
 
 const WindowEvent = () => {
-  const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(1);
 
   const addPElement = () => {
     if (counter > 7) {
       const deleteThem = document.querySelectorAll(".children");
       deleteThem.forEach((kid) => kid.remove());
-      setCounter(0);
+      setCounter(1);
     } else {
       const daddy = document.querySelector(".daddy");
       const child = document.createElement("p");
       child.setAttribute("class", "children");
-      child.innerHTML = "Another Child";
+      child.innerHTML = `Another Child ${counter}`;
       daddy.appendChild(child);
       setCounter((prevCounter) => prevCounter + 1);
     }
