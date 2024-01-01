cf72e76a5ebc495e7642d0fc2181b1abba0d326e
Johan Swan <jswannieman@gmail.com>
2024-01-01 15:32:04 +1300

    Update WindowEvent JSX Component

--- src/WindowEvent.jsx
+++ src/WindowEvent.jsx
@@ -3,6 +3,9 @@ import { useState, useEffect } from "react";
 const WindowEvent = () => {
   const [counter, setCounter] = useState(1);
   const [counter2, setCounter2] = useState(1);
+  const [bg, setBg] = useState("black");
+
+  const colors = ["red", "navy", "rebeccapurple", "green", "purple", "orange"];
 
   const addPElement = () => {
     if (counter > 7) {
@@ -10,6 +13,8 @@ const WindowEvent = () => {
       deleteThem.forEach((kid) => kid.remove());
       setCounter(1);
       setCounter2((prevCounter2) => prevCounter2 + 1);
+      const newBackground = colors[Math.floor(Math.random() * colors.length)];
+      setBg(newBackground);
     } else {
       const daddy = document.querySelector(".daddy");
       const child = document.createElement("p");
@@ -28,7 +33,17 @@ const WindowEvent = () => {
 
   return (
     <div className="daddy">
-      THE WINDOW EVENT IS NOW ACTIVE - ROUND NUMBER {counter2}
+      THE WINDOW EVENT IS NOW ACTIVE -{" "}
+      <span
+        id="spanner"
+        style={{
+          backgroundColor: `${bg}`,
+          color: "white",
+          padding: "1rem",
+        }}
+      >
+        ROUND NUMBER {counter2}
+      </span>
     </div>
   );
 };
