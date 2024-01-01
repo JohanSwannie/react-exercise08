d9e2c8f4cfa934c5b3f9569e130058700c0a8a7c
Johan Swan <jswannieman@gmail.com>
2024-01-01 14:58:54 +1300

    Update WindowEvent JSX Component

--- src/WindowEvent.jsx
+++ src/WindowEvent.jsx
@@ -2,12 +2,14 @@ import { useState, useEffect } from "react";
 
 const WindowEvent = () => {
   const [counter, setCounter] = useState(1);
+  const [counter2, setCounter2] = useState(1);
 
   const addPElement = () => {
     if (counter > 7) {
       const deleteThem = document.querySelectorAll(".children");
       deleteThem.forEach((kid) => kid.remove());
       setCounter(1);
+      setCounter2((prevCounter2) => prevCounter2 + 1);
     } else {
       const daddy = document.querySelector(".daddy");
       const child = document.createElement("p");
@@ -24,7 +26,11 @@ const WindowEvent = () => {
     return () => window.removeEventListener("dblclick", doubleClick);
   });
 
-  return <div className="daddy">THE WINDOW EVENT IS NOW ACTIVE !</div>;
+  return (
+    <div className="daddy">
+      THE WINDOW EVENT IS NOW ACTIVE - ROUND NUMBER {counter2}
+    </div>
+  );
 };
 
 export default WindowEvent;
